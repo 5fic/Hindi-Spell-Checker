0e165a2173a1ecf0cc47626d16f752e73d71ec52
5fic <sfic@email.com>
2016-03-08 18:40:38 +0530

    Update check.js

--- check.js
+++ check.js
@@ -2,4 +2,7 @@ function Check_Type( )
 {
 // Type of words, nown, pronoun etc.
 
+var vvalue = document.getElementById("hsc-vbox").value  ; //for hidden box
+var text_size = document.getElementById("hsc-box").value.length ; //for edit box,
+
 }
